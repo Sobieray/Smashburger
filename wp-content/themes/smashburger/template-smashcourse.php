9e9cf67a16d6e092f7661204fab0ae54868e7858
Matt Sobieray <mattsobieray@gmail.com>
2015-02-19 11:30:51 -0700

    updates to smash course

--- wp-content/themes/smashburger/template-smashcourse.php
+++ wp-content/themes/smashburger/template-smashcourse.php
@@ -31,42 +31,42 @@ get_header(); ?>
 				<p>Creates a</br>non-stick barrier.</p>
 			</div>
 		
-			<img id="angus-logo" data-3000="opacity:0;" data-4000="opacity:1;" data-4000="opacity:1;" data-8799="opacity:1;" data-8800="opacity:0;" src="<?php echo get_template_directory_uri() . '/img/angus_beef_logo.png' ?>" alt="smashburger certifited angus beef">
+			<img id="angus-logo" class="smash-opacity" data-3000="opacity:0;" data-4000="opacity:1;" data-4000="opacity:1;" data-8799="opacity:1;" data-8800="opacity:0;" src="<?php echo get_template_directory_uri() . '/img/angus_beef_logo.png' ?>" alt="smashburger certifited angus beef">
 			<div id="mask" data-8100="height: 0%;" data-8750="height: 100%;" data-8799="opacity:1;" data-8800="opacity:0;"></div>
 			<div id="the-grill" class="red-secondary white-text" data-3500="opacity:0;" data-4000="opacity:1;" data-4500="opacity:0;">
 				<h3>&bullet; The Beef &bullet;</h3>
 				<p>When you place your order, we place a fresh, never frozen, loosely packed ball of Certified Angus Beef&reg; on that buttered flat grill.</p>
 			</div>
-			<div id="flavor" data-4500="opacity:0;" data-5000="opacity:1;" data-5500="opacity:0;">
-				<p>Buttery, rich, flavor profile.</p>
+			<div id="flavor" class="smash-bg smash-opacity" data-4500="opacity:0;" data-5000="opacity:1;" data-5500="opacity:0;">
+				<p class="smashtext smash-padding">Buttery, rich, flavor profile.</p>
 			</div>
-			<div id="pasture" data-5500="opacity:0;" data-6000="opacity:1;" data-6500="opacity:0;">
-				<p>Pasture fed &amp; corn finished.</p>
+			<div id="pasture" class="smash-bg smash-opacity" data-5500="opacity:0;" data-6000="opacity:1;" data-6500="opacity:0;">
+				<p class="smashtext smash-padding">Pasture fed &amp; corn finished.</p>
 			</div>
-			<div id="frozen" data-6000="opacity:0;" data-6500="opacity:1;" data-7000="opacity:0;">
-				<p>Fresh, never frozen.</p>
+			<div id="frozen" class="smash-bg smash-opacity" data-6000="opacity:0;" data-6500="opacity:1;" data-7000="opacity:0;">
+				<p class="smashtext smash-padding">Fresh, never frozen.</p>
 			</div>
-			<div id="natural" data-6500="opacity:0;" data-7000="opacity:1;" data-7500="opacity:0;">
-				<p>All natural, of course.</p>
+			<div id="natural"  class="smash-bg smash-opacity" data-6500="opacity:0;" data-7000="opacity:1;" data-7500="opacity:0;">
+				<p class="smashtext smash-padding">All natural, of course.</p>
 			</div>
 			<img id="steam" data-0 ="opacity: 0;" data-8500="opacity:0;" data-8800="opacity: 1;" data-12900="opacity:1;" data-13500="opacity:0;" src="<?php echo get_template_directory_uri() . '/img/steam.png' ?>" alt="smashburger smasher steam">
-			<img id="burger-paddy" data-12900="opacity:0;" data-13000="opacity:1;" data-20300="top: 48%;" data-20500="top: 46%;" src="<?php echo get_template_directory_uri() . '/img/smashcourse_burger_paddy.png' ?>" alt="smashcourse burger paddy">
+			<img id="burger-paddy" class="smash-opacity" data-12900="opacity:0;" data-13000="opacity:1;" data-20300="top: 48%;" data-20500="top: 46%;" src="<?php echo get_template_directory_uri() . '/img/smashcourse_burger_paddy.png' ?>" alt="smashcourse burger paddy">
 			<img id="smasher" data-0 ="opacity: 0;" data-7500="top: -100%; opacity: 1;" data-8500="top: 32%;" data-12900="top: 32%; opacity:1;" data-13500="top:-100%; opacity:0;" src="<?php echo get_template_directory_uri() . '/img/smashcourse_smasher.png' ?>" alt="smashburger burger smasher">
 			<div id="the-grill" class="red-secondary white-text" data-8500="opacity:0;" data-9000="opacity:1;" data-9500="opacity:0;">
 				<h3>&bullet; The Smash &bullet;</h3>
 				<p>The perfect smash is the secret.  The right amount and type of meat against the perfect amount of heat, for just the right amount of time, locks in the juices.</p>
 			</div>
-			<div id="frozen" data-9500="opacity:0;" data-10000="opacity:1;" data-10500="opacity:0;">
-				<p>The Smasher is shaping the perfect patty.</p>
+			<div id="frozen" class="smash-bg smash-opacity" data-9500="opacity:0;" data-10000="opacity:1;" data-10500="opacity:0;">
+				<p class="smashtext smash-padding">The Smasher is shaping the perfect patty.</p>
 			</div>
-			<div id="smash-time" data-10000="opacity:0;" data-10500="opacity:1;" data-11000="opacity:0;">
-				<p>The Smash lasts for 10 seconds. No more, no less.</p>
+			<div id="smash-time" class="smash-bg smash-opacity" data-10000="opacity:0;" data-10500="opacity:1;" data-11000="opacity:0;">
+				<p class="smashtext smash-padding">The Smash lasts for 10 seconds. No more, no less.</p>
 			</div>
-			<div id="seared" data-11000="opacity:0;" data-11500="opacity:1;" data-12000="opacity:0;">
-				<p>The bottom of the burger is being seared to create a crispy, caramelized shell.</p>
+			<div id="seared" class="smash-bg smash-opacity" class="smash-opacity" data-11000="opacity:0;" data-11500="opacity:1;" data-12000="opacity:0;">
+				<p class="smashtext smash-padding">The bottom of the burger is being seared to create a crispy, caramelized shell.</p>
 			</div>
-			<div id="heatup" data-12000="opacity:0;" data-12500="opacity:1;" data-13000="opacity:0;">
-				<p>The heat pushes the juices up, not out.</p>
+			<div id="heatup" class="smash-bg smash-opacity" data-12000="opacity:0;" data-12500="opacity:1;" data-13000="opacity:0;">
+				<p class="smashtext smash-padding">The heat pushes the juices up, not out.</p>
 			</div>
 			<img id="arrow1" class="arrow" data-12000="opacity: 0; width:0px;" data-12500="opacity:1; width:70px;" data-12600="opacity:0;" src="<?php echo get_template_directory_uri() . '/img/smashcourse_arrow.svg' ?>" alt="smashcourse arrows">
 			<img id="arrow2" class="arrow" data-12000="opacity: 0; width:0px;" data-12600="opacity:1; width:70px;" data-12700="opacity:0;" src="<?php echo get_template_directory_uri() . '/img/smashcourse_arrow.svg' ?>" alt="smashcourse arrows">
@@ -77,28 +77,28 @@ get_header(); ?>
 				<h3>&bullet; The Seasoning &bullet;</h3>
 				<p>The burger is then seasoned to perfection with what we like to call flavor amplifier.</p>
 			</div>
-			<div id="salt" data-14500="opacity:0;" data-15000="opacity:1;" data-15500="opacity:0;">
-				<p>Kosher salt, black pepper, garlic.</p>
+			<div id="salt" class="smash-bg smash-opacity" data-14500="opacity:0;" data-15000="opacity:1;" data-15500="opacity:0;">
+				<p class="smashtext smash-padding">Kosher salt, black pepper, garlic.</p>
 			</div>
-			<div id="secret" data-15400="opacity:0;" data-15500="opacity:1;" data-16000="opacity:0;">
-				<p>Add a bit of "proprietary natural magic" (It's a secret).</p>
+			<div id="secret" class="smash-bg smash-opacity" data-15400="opacity:0;" data-15500="opacity:1;" data-16000="opacity:0;">
+				<p class="smashtext smash-padding">Add a bit of "proprietary natural magic" (It's a secret).</p>
 			</div>
-			<div id="dissolve" data-15900="opacity:0;" data-16000="opacity:1;" data-16500="opacity:0;">
-				<p>The juices dissolve the seasonings and pull them deep into the burger creating that distinctive Smashburger flavor.</p>
+			<div id="dissolve" class="smash-bg smash-opacity" data-15900="opacity:0;" data-16000="opacity:1;" data-16500="opacity:0;">
+				<p class="smashtext smash-padding">The juices dissolve the seasonings and pull them deep into the burger creating that distinctive Smashburger flavor.</p>
 			</div>
 			<div id="the-grill" class="red-secondary white-text" data-16500="opacity:0;" data-17000="opacity:1;" data-17500="opacity:0;">
 				<h3>&bullet; The Artisan Bun &bullet;</h3>
 				<p>Even the bun has been expertly prepared and plays an important role in the perfect burger.</p>
 			</div>
-			<img id="bun-top" class="buns" data-16500="opacity:0; top:-20%;" data-17000="opacity:1; top: 6%;" data-20300="top: 6%;" data-20500="top:14%;" src="<?php echo get_template_directory_uri() . '/img/bun_top.png' ?>">
-			<img id="bun-bottom" class="buns" data-16500="opacity:0; bottom:-20%;" data-17000="opacity:1; bottom: 29%;" data-20300="bottom: 29%;" data-20500="bottom:33%;" src="<?php echo get_template_directory_uri() . '/img/bun_bottom.png' ?>">
-			<div id="laser" data-17500="opacity:0;" data-18000="opacity:1;" data-18500="opacity:0;">
-				<p>It’s cut perfectly with laser precision every time.</p>
+			<img id="bun-top" class="buns" data-16500="opacity:0; top:-20%; display:none;" data-16510="display:block;" data-17000="opacity:1; top: 6%;" data-20300="top: 6%;" data-20500="top:14%;" src="<?php echo get_template_directory_uri() . '/img/bun_top.png' ?>">
+			<img id="bun-bottom" class="buns smash-opacity" data-16500="opacity:0; bottom:-20%;" data-17000="opacity:1; bottom: 29%;" data-20300="bottom: 29%;" data-20500="bottom:33%;" src="<?php echo get_template_directory_uri() . '/img/bun_bottom.png' ?>">
+			<div id="laser" class="smash-bg smash-opacity" data-17500="opacity:0;" data-18000="opacity:1;" data-18500="opacity:0;">
+				<p class="smashtext smash-padding">It’s cut perfectly with laser precision every time.</p>
 			</div>
-			<div id="toast" class="smash-bg smash-opacity" data-18000="opacity:0;" data-18500="opacity:1;" data-19000="opacity:0;">
+			<div id="toast" class="smash-bg smash-opacity" class="smash-bg smash-opacity smash-opacity" data-18000="opacity:0;" data-18500="opacity:1;" data-19000="opacity:0;">
 				<p class="smashtext smash-padding">We butter toast with pressure.</p>
 			</div>
-			<div id="sealed" class="smash-bg smash-opacity" data-18500="opacity:0;" data-19000="opacity:1;" data-19500="opacity:0;">
+			<div id="sealed" class="smash-bg smash-opacity" class="smash-bg smash-opacity smash-opacity" data-18500="opacity:0;" data-19000="opacity:1;" data-19500="opacity:0;">
 				<p class="smashtext smash-padding">Which creates a sealed, caramelized surface that keeps all of the juices in the burger.</p>
 			</div>
 			<img id="accessories" class="smash-opacity" data-19500="opacity:0;" data-20000="opacity:1;" src="<?php echo get_template_directory_uri() . '/img/accesories.png' ?>">
@@ -107,20 +107,24 @@ get_header(); ?>
 				<p>Then as if that wasn't good enough, we top your burger with the freshest ingredients.</p>
 			</div>
 			<img id="flavor-pin" class="smash-opacity" data-20400="opacity:0;" data-20800="opacity:1;" src="<?php echo get_template_directory_uri() . '/img/smashcourse_pin.png' ?>" alt="smashcourse flavor pin">
+			<img id="spiral" class="smash-opacity" data-20400="opacity:0;" data-20800="opacity:1;" src="<?php echo get_template_directory_uri() . '/img/smash_sprial_bg.png' ?>" alt="smashcourse flavor pin">
+			<div id="bon" class="smash-bg smash-opacity" class="smash-bg smash-opacity smash-opacity" data-20400="opacity:0;" data-20800="opacity:1;">
+				<p class="smashtext smash-padding">Bon Appetit!</p>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	
 <?php get_footer(); ?>
 <script type="text/javascript">
-	var s = skrollr.init({
-	/*	edgeStrategy: 'set',
+	var s = skrollr.init(
+		/*edgeStrategy: 'set',
 		easing: {
 			WTF: Math.random,
 			inverted: function(p) {
 				return 1-p;
 			}
 		}*/
-	});
+	);
 	function windowHieght() {
 		var height= $(window).height();
 		$('#primary').css({height: height});
