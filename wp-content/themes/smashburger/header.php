c756a8462806fc8d1574cebd7202b9a190fa337d
Matt Sobieray <mattsobieray@gmail.com>
2015-02-27 10:37:45 -0700

    added ie css fixes for food-story

--- wp-content/themes/smashburger/header.php
+++ wp-content/themes/smashburger/header.php
@@ -17,7 +17,7 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
 <?php wp_head(); ?>
-<!--[if lt IE 11]>
+<!--[if lt *NA-IE 11]>
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/css/ie9-and-down.css' ?>" />
 <![endif]-->
 </head>
