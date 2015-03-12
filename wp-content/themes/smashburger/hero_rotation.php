cc9917901d1477d41d03199cb2ea047c09dabb60
Matt Sobieray <mattsobieray@gmail.com>
2015-03-12 12:56:43 -0600

    start of homepage restructure, Updated Location Widget

--- wp-content/themes/smashburger/hero_rotation.php
+++ wp-content/themes/smashburger/hero_rotation.php
@@ -1,16 +1,28 @@
-<div id="deserveit" class="parallax1 flex rotation" data-speed="7" data-offsetY="0" data-type="background">
+<div id="deserveit" class="parallax1 flex rotation" data-speed="7" data-offsetY="0" data-type="background" >
 	<span class="red-primary"></span>
 	<!-- <div class="lifestyle" data-type="sprite" data-offsetY="200" data-Xposition="30%" data-speed="-3"></div> -->
 	<div id="deservit-burger" class="burger" data-type="sprite" data-offsetY="92" data-Xposition="110%" data-speed="9"></div>
 	<p>becauseideserveit<span class="redtext-primary">burger</span></p>
 	<div class="avocado" data-type="sprite" data-offsetY="600" data-Xposition="110%" data-speed="4"></div>
 	<div class="jalapeno" data-type="sprite" data-offsetY="700" data-Xposition="90%" data-speed="2"></div>	
-	<aside class="small-8 column no-padding-left location-widget" width="50%">
-		<h2>your nearest smashburger location</h2>
-		<div class="placeable small-6 column no-padding">
+	<aside class="small-12 no-padding location-widget" data-equalizer>
+		<div class="placeable small-6 column no-padding" data-equalizer-watch>
+			<h2>your nearest smashburger location</h2>
 			<img src="<?php echo get_template_directory_uri() . '/img/loader_widget.gif'; ?>" alt="smashburger loading image"/>
 		</div>
-		<div class="placeable-img small-6 column no-padding">
+		<div class="placeable-img small-6 column no-padding" data-equalizer-watch>
+			<div class="hero-form clear">
+				<form action="http://smashburger.fbmta.com/members/subscribe.aspx" method="post" name="smashclub-form-global" id="smashclub-form-global" onSubmit="return validateEmail(this.EmailAddress.value);">
+					<input type="hidden" name="Action" id="Action" value="subscribe" size="30" maxlength="200">
+					<input type="hidden" value="45097156610" name="ListID">
+					<input type="hidden" value="4a1f3487-679e-4ca6-bccc-0ee2fb40dab5 " name="SiteGUID">  
+					<input type="hidden" value="https://smashburger.com/thank-you/" name="ReturnURL">
+					<input type="hidden" value="we" name="InputSource">
+			    <input type="text" value="join the smashclub!" name="EmailAddress" maxlength="200" id="EmailAddress">
+					<input type="submit" name="submit" value="GO!" class="form-submit">
+				</form>
+				<i>By signing up for the Smashclub, you are verifying that you are over the age of 13.</i>
+			</div>
 			<img src="<?php echo get_template_directory_uri() . '/img/store_front.jpg'; ?>" alt="smashburger interior"/>
 		</div>
 		<div class="lettuce" data-type="sprite" data-offsetY="566" data-Xposition="0%" data-speed="4"></div>
@@ -24,16 +36,28 @@
 	<p>workhardplayhard<span class="redtext-primary">burger</span></p>
 	<div class="avocado" data-type="sprite" data-offsetY="600" data-Xposition="110%" data-speed="4"></div>
 	<div class="jalapeno" data-type="sprite" data-offsetY="700" data-Xposition="90%" data-speed="2"></div>	
-	<aside class="small-8 column no-padding-left location-widget" width="50%">
-		<h2>your nearest smashburger location</h2>
-		<div class="placeable small-6 column no-padding">
+	<aside class="small-12 no-padding location-widget" data-equalizer>
+		<div class="placeable small-6 column no-padding" data-equalizer-watch>
+			<h2>your nearest smashburger location</h2>
 			<img src="<?php echo get_template_directory_uri() . '/img/loader_widget.gif'; ?>" alt="smashburger loading image"/>
 		</div>
-		<div class="placeable-img small-6 column no-padding">
+		<div class="placeable-img small-6 column no-padding" data-equalizer-watch>
+			<div class="hero-form clear">
+				<form action="http://smashburger.fbmta.com/members/subscribe.aspx" method="post" name="smashclub-form-global" id="smashclub-form-global" onSubmit="return validateEmail(this.EmailAddress.value);">
+					<input type="hidden" name="Action" id="Action" value="subscribe" size="30" maxlength="200">
+					<input type="hidden" value="45097156610" name="ListID">
+					<input type="hidden" value="4a1f3487-679e-4ca6-bccc-0ee2fb40dab5 " name="SiteGUID">  
+					<input type="hidden" value="https://smashburger.com/thank-you/" name="ReturnURL">
+					<input type="hidden" value="we" name="InputSource">
+			    <input type="text" value="join the smashclub!" name="EmailAddress" maxlength="200" id="EmailAddress">
+					<input type="submit" name="submit" value="GO!" class="form-submit">
+				</form>
+				<i>By signing up for the Smashclub, you are verifying that you are over the age of 13.</i>
+			</div>
 			<img src="<?php echo get_template_directory_uri() . '/img/store_front.jpg'; ?>" alt="smashburger interior"/>
 		</div>
 		<div class="lettuce" data-type="sprite" data-offsetY="566" data-Xposition="0%" data-speed="4"></div>
-	</aside>	
+	</aside>		
 </div>
 
 <div id="fullest1" class="parallax1 flex rotation" data-speed="7" data-offsetY="0" data-type="background">
@@ -43,12 +67,24 @@
 	<p>livelifetothefullest<span class="redtext-primary">burger</span></p>
 	<div class="avocado" data-type="sprite" data-offsetY="600" data-Xposition="110%" data-speed="4"></div>
 	<div class="jalapeno" data-type="sprite" data-offsetY="700" data-Xposition="90%" data-speed="2"></div>	
-	<aside class="small-8 column no-padding-left location-widget" width="50%">
-		<h2>your nearest smashburger location</h2>
-		<div class="placeable small-6 column no-padding">
+	<aside class="small-12 no-padding location-widget" data-equalizer>
+		<div class="placeable small-6 column no-padding" data-equalizer-watch>
+			<h2>your nearest smashburger location</h2>
 			<img src="<?php echo get_template_directory_uri() . '/img/loader_widget.gif'; ?>" alt="smashburger loading image"/>
 		</div>
-		<div class="placeable-img small-6 column no-padding">
+		<div class="placeable-img small-6 column no-padding" data-equalizer-watch>
+			<div class="hero-form clear">
+				<form action="http://smashburger.fbmta.com/members/subscribe.aspx" method="post" name="smashclub-form-global" id="smashclub-form-global" onSubmit="return validateEmail(this.EmailAddress.value);">
+					<input type="hidden" name="Action" id="Action" value="subscribe" size="30" maxlength="200">
+					<input type="hidden" value="45097156610" name="ListID">
+					<input type="hidden" value="4a1f3487-679e-4ca6-bccc-0ee2fb40dab5 " name="SiteGUID">  
+					<input type="hidden" value="https://smashburger.com/thank-you/" name="ReturnURL">
+					<input type="hidden" value="we" name="InputSource">
+			    <input type="text" value="join the smashclub!" name="EmailAddress" maxlength="200" id="EmailAddress">
+					<input type="submit" name="submit" value="GO!" class="form-submit">
+				</form>
+				<i>By signing up for the Smashclub, you are verifying that you are over the age of 13.</i>
+			</div>
 			<img src="<?php echo get_template_directory_uri() . '/img/store_front.jpg'; ?>" alt="smashburger interior"/>
 		</div>
 		<div class="lettuce" data-type="sprite" data-offsetY="566" data-Xposition="0%" data-speed="4"></div>
@@ -62,16 +98,28 @@
 	<p>nothingbutthebest<span class="redtext-primary">burger</span></p>
 	<div class="avocado" data-type="sprite" data-offsetY="600" data-Xposition="110%" data-speed="4"></div>
 	<div class="jalapeno" data-type="sprite" data-offsetY="700" data-Xposition="90%" data-speed="2"></div>	
-	<aside class="small-8 column no-padding-left location-widget" width="50%">
-		<h2>your nearest smashburger location</h2>
-		<div class="placeable small-6 column no-padding">
+	<aside class="small-12 no-padding location-widget" data-equalizer>
+		<div class="placeable small-6 column no-padding" data-equalizer-watch>
+			<h2>your nearest smashburger location</h2>
 			<img src="<?php echo get_template_directory_uri() . '/img/loader_widget.gif'; ?>" alt="smashburger loading image"/>
 		</div>
-		<div class="placeable-img small-6 column no-padding">
+		<div class="placeable-img small-6 column no-padding" data-equalizer-watch>
+			<div class="hero-form clear">
+				<form action="http://smashburger.fbmta.com/members/subscribe.aspx" method="post" name="smashclub-form-global" id="smashclub-form-global" onSubmit="return validateEmail(this.EmailAddress.value);">
+					<input type="hidden" name="Action" id="Action" value="subscribe" size="30" maxlength="200">
+					<input type="hidden" value="45097156610" name="ListID">
+					<input type="hidden" value="4a1f3487-679e-4ca6-bccc-0ee2fb40dab5 " name="SiteGUID">  
+					<input type="hidden" value="https://smashburger.com/thank-you/" name="ReturnURL">
+					<input type="hidden" value="we" name="InputSource">
+			    <input type="text" value="join the smashclub!" name="EmailAddress" maxlength="200" id="EmailAddress">
+					<input type="submit" name="submit" value="GO!" class="form-submit">
+				</form>
+				<i>By signing up for the Smashclub, you are verifying that you are over the age of 13.</i>
+			</div>
 			<img src="<?php echo get_template_directory_uri() . '/img/store_front.jpg'; ?>" alt="smashburger interior"/>
 		</div>
 		<div class="lettuce" data-type="sprite" data-offsetY="566" data-Xposition="0%" data-speed="4"></div>
-	</aside>	
+	</aside>		
 </div>
 
 <div id="date" class="parallax1 flex rotation" data-speed="7" data-offsetY="0" data-type="background">
@@ -81,16 +129,28 @@
 	<p>finallyadatenight<span class="redtext-primary">burger</span></p>
 	<div class="avocado" data-type="sprite" data-offsetY="600" data-Xposition="110%" data-speed="4"></div>
 	<div class="jalapeno" data-type="sprite" data-offsetY="700" data-Xposition="90%" data-speed="2"></div>	
-	<aside class="small-8 column no-padding-left location-widget" width="50%">
-		<h2>your nearest smashburger location</h2>
-		<div class="placeable small-6 column no-padding">
+	<aside class="small-12 no-padding location-widget" data-equalizer>
+		<div class="placeable small-6 column no-padding" data-equalizer-watch>
+			<h2>your nearest smashburger location</h2>
 			<img src="<?php echo get_template_directory_uri() . '/img/loader_widget.gif'; ?>" alt="smashburger loading image"/>
 		</div>
-		<div class="placeable-img small-6 column no-padding">
+		<div class="placeable-img small-6 column no-padding" data-equalizer-watch>
+			<div class="hero-form clear">
+				<form action="http://smashburger.fbmta.com/members/subscribe.aspx" method="post" name="smashclub-form-global" id="smashclub-form-global" onSubmit="return validateEmail(this.EmailAddress.value);">
+					<input type="hidden" name="Action" id="Action" value="subscribe" size="30" maxlength="200">
+					<input type="hidden" value="45097156610" name="ListID">
+					<input type="hidden" value="4a1f3487-679e-4ca6-bccc-0ee2fb40dab5 " name="SiteGUID">  
+					<input type="hidden" value="https://smashburger.com/thank-you/" name="ReturnURL">
+					<input type="hidden" value="we" name="InputSource">
+			    <input type="text" value="join the smashclub!" name="EmailAddress" maxlength="200" id="EmailAddress">
+					<input type="submit" name="submit" value="GO!" class="form-submit">
+				</form>
+				<i>By signing up for the Smashclub, you are verifying that you are over the age of 13.</i>
+			</div>
 			<img src="<?php echo get_template_directory_uri() . '/img/store_front.jpg'; ?>" alt="smashburger interior"/>
 		</div>
 		<div class="lettuce" data-type="sprite" data-offsetY="566" data-Xposition="0%" data-speed="4"></div>
-	</aside>	
+	</aside>		
 </div>
 
 <div id="fullest2" class="parallax1 flex rotation" data-speed="7" data-offsetY="0" data-type="background">
@@ -100,14 +160,26 @@
 	<p>livelifetothefullest<span class="redtext-primary">burger</span></p>
 	<div class="avocado" data-type="sprite" data-offsetY="600" data-Xposition="110%" data-speed="4"></div>
 	<div class="jalapeno" data-type="sprite" data-offsetY="700" data-Xposition="90%" data-speed="2"></div>	
-	<aside class="small-8 column no-padding-left location-widget" width="50%">
-		<h2>your nearest smashburger location</h2>
-		<div class="placeable small-6 column no-padding">
+	<aside class="small-12 no-padding location-widget" data-equalizer>
+		<div class="placeable small-6 column no-padding" data-equalizer-watch>
+			<h2>your nearest smashburger location</h2>
 			<img src="<?php echo get_template_directory_uri() . '/img/loader_widget.gif'; ?>" alt="smashburger loading image"/>
 		</div>
-		<div class="placeable-img small-6 column no-padding">
+		<div class="placeable-img small-6 column no-padding" data-equalizer-watch>
+			<div class="hero-form clear">
+				<form action="http://smashburger.fbmta.com/members/subscribe.aspx" method="post" name="smashclub-form-global" id="smashclub-form-global" onSubmit="return validateEmail(this.EmailAddress.value);">
+					<input type="hidden" name="Action" id="Action" value="subscribe" size="30" maxlength="200">
+					<input type="hidden" value="45097156610" name="ListID">
+					<input type="hidden" value="4a1f3487-679e-4ca6-bccc-0ee2fb40dab5 " name="SiteGUID">  
+					<input type="hidden" value="https://smashburger.com/thank-you/" name="ReturnURL">
+					<input type="hidden" value="we" name="InputSource">
+			    <input type="text" value="join the smashclub!" name="EmailAddress" maxlength="200" id="EmailAddress">
+					<input type="submit" name="submit" value="GO!" class="form-submit">
+				</form>
+				<i>By signing up for the Smashclub, you are verifying that you are over the age of 13.</i>
+			</div>
 			<img src="<?php echo get_template_directory_uri() . '/img/store_front.jpg'; ?>" alt="smashburger interior"/>
 		</div>
 		<div class="lettuce" data-type="sprite" data-offsetY="566" data-Xposition="0%" data-speed="4"></div>
-	</aside>	
+	</aside>		
 </div>
