622656e48f9c9f53a1c78a9e6da622cb3cafcfc3
Matt Sobieray <mattsobieray@gmail.com>
2015-03-17 09:54:27 -0600

    updated 404 page

--- wp-content/themes/smashburger/404.php
+++ wp-content/themes/smashburger/404.php
@@ -14,15 +14,18 @@ get_header(); ?>
 				<header class="page-header">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'smashburger' ); ?></h1>
 				</header><!-- .page-header -->
+				<div class="header"><h1>Uh-Oh! We Can&rsquo;t Find This Page</h1></div>
+				<div class="page-content" style="background-color: #343334; margin-top: 0;">
+					<div class ="row">
+						<p style="margin-top: 2em;" class="center white-text"><?php _e( 'It looks like nothing was found at this location' ); ?></p>
+						<iframe style="display:block;" class="center" src="//giphy.com/embed/WBTUlyKBVlTTa?html5=true" width="480" height="480" frameBorder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+					</div>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'smashburger' ); ?></p>
-
-					<?php get_search_form(); ?>
+					<!-- <?php get_search_form(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php if ( smashburger_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+					<?php if ( smashburger_categorized_blog() ) : // Only show the widget if site has multiple categories. ?> 
 					<div class="widget widget_categories">
 						<h2 class="widget-title"><?php _e( 'Most Used Categories', 'smashburger' ); ?></h2>
 						<ul>
@@ -36,7 +39,7 @@ get_header(); ?>
 							) );
 						?>
 						</ul>
-					</div><!-- .widget -->
+					</div><!-- .widget 
 					<?php endif; ?>
 
 					<?php
