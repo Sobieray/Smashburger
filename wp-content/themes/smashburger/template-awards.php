7c0a0c84ecef7885be2684607d9b720dce28ab62
Matt Sobieray <mattsobieray@gmail.com>
2015-04-13 10:00:42 -0600

    fine tuning the ffeatured awards css and html

--- wp-content/themes/smashburger/template-awards.php
+++ wp-content/themes/smashburger/template-awards.php
@@ -16,15 +16,15 @@ get_header(); ?>
 				<h2 class="white-text red-secondary">featured awards</h2>
 			</span>
 			<div data-equalizer class="clear">
-				<a href="http://www.franchisetimes.com/March-2015/Fast-and-Serious/#Smashburger">
-					<div class="small-12 medium-6 column dark-blue-bg no-padding" data-equalizer-watch>
+				<a class="small-12 medium-6 column dark-blue-bg no-padding" href="http://www.franchisetimes.com/March-2015/Fast-and-Serious/#Smashburger" target="_blank">
+					<div data-equalizer-watch>
 						<span>2015</span>
 						<h3 class="blue-bg white-text">#3 &ldquo;List of smartest-growing brands&rdquo;</h3>
-						<h3 class="blue-bg white-text">Fast &amp; Serious</h3>
+						<h3 class="blue-bg white-text">Fast &amp; Serious</h3>	
 					</div>
-				</a>
-				<a href="http://www.fastcasual.com/articles/smashburger-named-top-brand-fazolis-ceo-named-top-exec-for-2014-fast-casual-top-100-movers-shakers/">
-					<div class="small-12 medium-6 column dark-blue-bg no-padding" data-equalizer-watch>
+				 </a>
+				<a class="small-12 medium-6 column dark-blue-bg no-padding" href="http://www.fastcasual.com/articles/smashburger-named-top-brand-fazolis-ceo-named-top-exec-for-2014-fast-casual-top-100-movers-shakers/" target="_blank">
+					<div  data-equalizer-watch>
 						<span>2014</span>
 						<h3 class="white-text">#1 on &ldquo;Top 100 Movers &amp; Shakers&rdquo; LIST </h3>
 						<h3 class="white-text">Fast Casual</h3>
@@ -32,74 +32,65 @@ get_header(); ?>
 				</a>
 			</div>
 			<div data-equalizer class="clear">
-				<a href="#">
-					<div class="small-12 medium-6 column dark-blue-bg no-padding" data-equalizer-watch>
-						<span>2012</span>
-						<h3 class="white-text">Best Franchise Deals</h3>
-						<h3 class="white-text">QSR Magazine</h3>
-					</div>
-				</a>
-				<a href="#">
-					<div class="small-12 medium-6 column dark-blue-bg no-padding" data-equalizer-watch>
-						<span>2011</span>
-						<h3 class="white-text">#6 &ldquo;America's most promising company&rdquo;</h3>
-						<h3 class="white-text">Forbes</h3>
-					</div>
-				</a>
+				<div class="small-12 medium-6 column dark-blue-bg no-padding" data-equalizer-watch>
+					<span>2012</span>
+					<h3 class="white-text">Best Franchise Deals</h3>
+					<h3 class="white-text">QSR Magazine</h3>
+				</div>
+				<div class="small-12 medium-6 column dark-blue-bg no-padding" data-equalizer-watch>
+					<span>2014</span>
+					<h3 class="white-text">#6 &ldquo;America's most promising company&rdquo;</h3>
+					<h3 class="white-text">Forbes</h3>
+				</div>
 			</div>
 		</main>
 		<div id="award_tiles">
 			<section class="1 center" data-equalizer>
-				<div class="small-12 medium-3 column tan-bg" data-equalizer-watch>
-					<p class="brown-text">Best of Boulder</p>
-					<h4 class="brown-text">Best Burger</h4>
-					<span>2014</span>
-				</div>
-				<a href="http://www.forbes.com/sites/brettnelson/2011/11/30/americas-most-promising-companies-the-top-20/"><div class="small-12 medium-3 column light-tan-bg" data-equalizer-watch>
+				<a href="http://www.forbes.com/sites/brettnelson/2011/11/30/americas-most-promising-companies-the-top-20/" target="_blank"><div class="small-12 medium-3 column light-tan-bg" data-equalizer-watch>
 					<p class="brown-text">Inc. 500|5000</p>
 					<h4 class="brown-text">featured on &ldquo;America&rsquo;s Fastest Growing Companies&rdquo;</h4>
 					<span>2014</span>
 				</div></a>
-				<a href="http://www.franchisetimes.com/March-2014/Smashburger/"><div class="small-12 medium-3 column red-secondary" data-equalizer-watch>
+				<a href="http://www.franchisetimes.com/March-2014/Smashburger/" target="_blank"><div class="small-12 medium-3 column red-secondary" data-equalizer-watch>
 					<p class="white-text">Franchise Times</p>
 					<h4 class="white-text">#2 smartest-growing brand ON &ldquo;2014 Fast &amp; Serious&rdquo; LIST</h4>
 					<span>2014</span>
 				</div></a>
-					<div class="small-12 medium-3 column blue-bg" data-equalizer-watch>
-					<p class="white-text">Certified Angus Beef&reg;</p>
-					<h4 class="white-text">Restaurant chain marketer of the year</h4>
+				<div class="small-12 medium-3 column tan-bg" data-equalizer-watch>
+					<p class="brown-text">Certified Angus Beef&reg;</p>
+					<h4 class="brown-text">Restaurant chain marketer of the year</h4>
 					<span>2014</span>
 				</div>
-			</section>
-			<section class="2 center" data-equalizer>
-				<a href="#"><div class="small-12 medium-3 column dark-blue-bg" data-equalizer-watch>
-					<p class="white-text">Forbes Magazine</p>
-					<h4 class="white-text">#6 &ldquo;America&rsquo;s most promising company&rdquo;</h4>
-					<span>2014</span>
-				</div></a>
-			  <div class="small-12 medium-3 column dark-blue-bg" data-equalizer-watch>
+				<div class="small-12 medium-3 column dark-blue-bg" data-equalizer-watch>
 					<p class="white-text"> Addison, TX </p>
 					<h4 class="white-text">favorite burger joint</h4>
 					<span>2013</span>
 				</div>
-				<div class="small-12 medium-3 column tan-bg" data-equalizer-watch>
-					<p class="brown-text">fast casual</p>
-					<h4 class="brown-text">small brand of the year</h4>
+			</section>
+			<section class="2 center" data-equalizer>
+				<div class="small-12 medium-3 column blue-bg" data-equalizer-watch>
+					<p class="white-text">fast casual</p>
+					<h4 class="white-text">small brand of the year</h4>
 					<span>2013</span>
 				</div>
-				<a href="http://www.inc.com/hire-power/list"><div class="small-12 medium-3 column blue-bg" data-equalizer-watch>
+				<div class="small-12 medium-3 column tan-bg" data-equalizer-watch>
+					<p class="brown-text">fast casual</p>
+					<h4 class="brown-text">top 50 brands of the year</h4>
+					<span>2013</span>
+				</div>
+				<a href="http://www.inc.com/hire-power/list" target="_blank"><div class="small-12 medium-3 column blue-bg" data-equalizer-watch>
 					<p class="white-text">Inc. 500|5000</p>
 					<h4 class="white-text">#9 on &ldquo;Top 100 job creators&rdquo;</h4>
 					<span>2012</span>
 				</div></a>
-			</section>
-			<section class="3 center" data-equalizer>
-					<div class="small-12 medium-3 column red-secondary" data-equalizer-watch>
+				<div class="small-12 medium-3 column red-secondary" data-equalizer-watch>
 					<p class="white-text">Entrepreneur Magazine</p>
 					<h4 class="white-text">Ranked #265 on franchise 500</h4>
 					<span>2012</span>
 				</div>
-				<a href="http://www.gourmet.com/food/gourmetlive/2012/080112/worlds-best-burgers"><div class="small-12 medium-3 column tan-bg" data-equalizer-watch>
+			</section>
+			<section class="3 center" data-equalizer>
+				<a href="http://www.gourmet.com/food/gourmetlive/2012/080112/worlds-best-burgers" target="_blank"><div class="small-12 medium-3 column tan-bg" data-equalizer-watch>
 					<p class="brown-text">gourmet magazine</p>
 					<h4 class="brown-text">Worlds Best Burger</h4>
 					<span>2012</span>
@@ -109,7 +100,12 @@ get_header(); ?>
 					<h4 class="brown-text">Best Burger</h4>
 					<span>2012</span>
 				</div>
-				<a href="http://www.reuters.com/article/2011/05/24/idUS235219+24-May-2011+BW20110524"><div class="small-12 medium-3 column dark-blue-bg" data-equalizer-watch>
+				<a href="http://www.forbes.com/sites/brettnelson/2011/11/30/americas-most-promising-companies-the-top-20/" target="_blank"><div class="small-12 medium-3 column dark-blue-bg" data-equalizer-watch>
+					<p class="white-text">Forbes Magazine</p>
+					<h4 class="white-text">&ldquo;America&rsquo;s most promising company&rdquo;</h4>
+					<span>2011</span>
+				</div></a>
+				<a href="http://www.reuters.com/article/2011/05/24/idUS235219+24-May-2011+BW20110524" target="_blank"><div class="small-12 medium-3 column blue-bg" data-equalizer-watch>
 					<p class="white-text">International Council of Shopping Centers</p>
 					<h4 class="white-text">Hot Retailer Award</h4>
 					<span>2011</span>
